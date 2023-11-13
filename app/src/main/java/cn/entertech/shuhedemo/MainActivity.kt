cff89f3b3c287b8d78630b186be8ff2de39fb444
wangkang <1226426603@qq.com>
2023-11-13 15:14:45 +0800

    移除无用代码

--- app/src/main/java/cn/entertech/shuhedemo/MainActivity.kt
+++ app/src/main/java/cn/entertech/shuhedemo/MainActivity.kt
@@ -1,15 +1,9 @@
 package cn.entertech.shuhedemo
 
-import android.Manifest
-import android.content.pm.PackageManager
-import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.ScrollView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import cn.entertech.affective.sdk.api.Callback
 import cn.entertech.affective.sdk.api.IAffectiveDataAnalysisService
 import cn.entertech.affective.sdk.api.IConnectionServiceListener
@@ -18,18 +12,14 @@ import cn.entertech.affective.sdk.api.IGetReportListener
 import cn.entertech.affective.sdk.api.IStartAffectiveServiceLister
 import cn.entertech.affective.sdk.bean.AffectiveServiceWay
 import cn.entertech.affective.sdk.bean.EnterAffectiveConfigProxy
-import cn.entertech.affective.sdk.bean.RealtimeAffectiveData
-import cn.entertech.affective.sdk.bean.RealtimeBioData
+import cn.entertech.affective.sdk.bean.Error
 import cn.entertech.affective.sdk.bean.UploadReportEntity
-import cn.entertech.affectivesdk.authentication.AuthenticationHelper
+import cn.entertech.affectivesdk.utils.SingleChannelEEGUtil
 import cn.entertech.affectivesdk.utils.SingleChannelEEGUtil.SINGLE_EGG_PCK_CHECK
 import cn.entertech.affectivesdk.utils.SingleChannelEEGUtil.SINGLE_EGG_PCK_END
 import cn.entertech.affectivesdk.utils.SingleChannelEEGUtil.SINGLE_EGG_PCK_START
-import cn.entertech.ble.cushion.CushionBleManager
-import cn.entertech.ble.single.BiomoduleBleManager
 import cn.entertech.shuhedemo.databinding.ActivityMainBinding
 import java.io.BufferedWriter
-import cn.entertech.affective.sdk.bean.Error
 import java.io.File
 import java.io.FileWriter
 import java.util.Random
@@ -37,43 +27,16 @@ import kotlin.concurrent.thread
 
 class MainActivity : AppCompatActivity() {
     private val TAG = "MainActivity"
-    private lateinit var biomoduleBleManager: BiomoduleBleManager
-    private lateinit var cushionBleManager: CushionBleManager
-    lateinit var binding: ActivityMainBinding
+    private lateinit var binding: ActivityMainBinding
     private val affectiveDataAnalysisService by lazy {
         IAffectiveDataAnalysisService.getService(AffectiveServiceWay.AffectiveLocalService)
     }
     private var screenText: String = ""
 
-    private val bdListener by lazy {
-        val bdListener: ((RealtimeBioData?) -> Unit) = {
-
-        }
-        bdListener
-    }
-
-    private val listener by lazy {
-        val bdListener: ((RealtimeAffectiveData?) -> Unit) = {
-
-        }
-        bdListener
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        initManager()
-        initPermission()
-    }
-
-    fun initManager() {
-        biomoduleBleManager = BiomoduleBleManager.getInstance(App.getInstance().applicationContext)
-        cushionBleManager = CushionBleManager.getInstance(App.getInstance().applicationContext)
-        biomoduleBleManager.addRawDataListener(rawDataListener)
-        cushionBleManager.addRawDataListener(rawCushionDataListener)
-        biomoduleBleManager.addHeartRateListener(hrDataListener)
-        affectiveDataAnalysisService?.subscribeData(bdListener, listener)
     }
 
     /**
@@ -136,116 +99,6 @@ class MainActivity : AppCompatActivity() {
         }*/
     }
 
-    fun byteArrayOfInts(vararg ints: Int) = ByteArray(ints.size) { pos -> ints[pos].toByte() }
-
-    fun onConnect(view: View) {
-        appendLog("正在扫描蓝牙...")
-        biomoduleBleManager.scanNearDeviceAndConnect(fun() {
-            appendLog("蓝牙扫描成功，正在连接...")
-            Log.d(TAG, "sacn success")
-        }, fun(e: Exception) {
-            appendLog("蓝牙扫描失败：$${e}")
-            Log.d(TAG, "sacn failed：$e")
-        }, fun(mac: String) {
-            appendLog("蓝牙连接成功$mac")
-            Log.d(TAG, "connect success$mac")
-            runOnUiThread {
-                Toast.makeText(this@MainActivity, "connect to device success", Toast.LENGTH_SHORT)
-                    .show()
-            }
-        }) { msg ->
-            Log.d(TAG, "connect failed")
-            appendLog("蓝牙连接失败")
-            runOnUiThread {
-                Toast.makeText(
-                    this@MainActivity,
-                    "failed to connect to device：${msg}",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        }
-    }
-
-    fun onConnectCushion(view: View) {
-        appendLog("正在扫描蓝牙...")
-        cushionBleManager.scanNearDeviceAndConnect(fun() {
-            appendLog("蓝牙扫描成功，正在连接...")
-            Log.d(TAG, "sacn success")
-        }, fun(e: Exception) {
-            appendLog("蓝牙扫描失败：$${e}")
-            Log.d(TAG, "sacn failed：$e")
-        }, fun(mac: String) {
-            appendLog("蓝牙连接成功$mac")
-            Log.d(TAG, "connect success$mac")
-            runOnUiThread {
-                Toast.makeText(this@MainActivity, "connect to device success", Toast.LENGTH_SHORT)
-                    .show()
-            }
-        }) { msg ->
-            Log.d(TAG, "connect failed")
-            appendLog("蓝牙连接失败")
-            runOnUiThread {
-                Toast.makeText(
-                    this@MainActivity,
-                    "failed to connect to device：${msg}",
-                    Toast.LENGTH_SHORT
-                ).show()
-            }
-        }
-    }
-
-    fun onDisconnect(view: View) {
-        biomoduleBleManager.disConnect()
-        cushionBleManager.disconnect()
-    }
-
-    /**
-     * 从硬件中读取到的脑波原始数据，通过appendEEG方法传入至情感算法SDK中
-     */
-    var rawDataListener = fun(eeg: ByteArray) {
-        affectiveDataAnalysisService?.apply {
-            if (hasStartAffectiveService()) {
-                appendEEGData(eeg)
-            }
-        }
-
-    }
-
-
-    /**
-     * 从硬件中读取到的脑波原始数据，通过appendEEG方法传入至情感算法SDK中
-     */
-    var rawCushionDataListener = fun(eeg: ByteArray) {
-        affectiveDataAnalysisService?.apply {
-            if (hasStartAffectiveService()) {
-                appendPEPRData(eeg)
-            }
-        }
-    }
-
-    /**
-     * 从硬件中读取到的心率原始数据，通过appendHR方法传入至情感算法SDK中
-     */
-    var hrDataListener = fun(hr: Int) {
-        affectiveDataAnalysisService?.apply {
-            if (hasStartAffectiveService()) {
-                appendHeartRateData(hr)
-            }
-        }
-    }
-
-    fun onStart(view: View) {
-        startAffectiveService {
-            biomoduleBleManager.startHeartAndBrainCollection()
-        }
-    }
-
-    fun onStartCushionData(view: View) {
-        startAffectiveService {
-            cushionBleManager.startCollection()
-        }
-    }
-
     private fun startAffectiveService(startCollection: (IAffectiveDataAnalysisService?) -> Unit) {
         affectiveDataAnalysisService?.apply {
             closeAffectiveServiceConnection()
@@ -287,7 +140,6 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onEnd(view: View) {
-        biomoduleBleManager.stopHeartAndBrainCollection()
         affectiveDataAnalysisService?.getReport(object : IGetReportListener {
             override fun getAffectiveReportError(error: Error?) {
                 appendLog("获取Affective报表数据失败：${error}")
@@ -397,45 +249,6 @@ class MainActivity : AppCompatActivity() {
            })*/
     }
 
-
-    /**
-     * Android6.0 auth
-     */
-    fun initPermission() {
-        val needPermission = if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
-            arrayOf(
-                Manifest.permission.BLUETOOTH_SCAN,
-                Manifest.permission.BLUETOOTH_CONNECT,
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            )
-        } else {
-            arrayOf(
-                Manifest.permission.WRITE_EXTERNAL_STORAGE,
-                Manifest.permission.READ_EXTERNAL_STORAGE,
-                Manifest.permission.ACCESS_FINE_LOCATION
-            )
-        }
-        val needRequestPermissions = ArrayList<String>()
-        for (i in needPermission.indices) {
-            if (ActivityCompat.checkSelfPermission(
-                    this,
-                    needPermission[i]
-                ) != PackageManager.PERMISSION_GRANTED
-            ) {
-                needRequestPermissions.add(needPermission[i])
-            }
-        }
-        if (needRequestPermissions.size != 0) {
-            val permissions = arrayOfNulls<String>(needRequestPermissions.size)
-            for (i in needRequestPermissions.indices) {
-                permissions[i] = needRequestPermissions[i]
-            }
-            ActivityCompat.requestPermissions(this@MainActivity, permissions, 1)
-        }
-    }
-
     fun appendLog(text: String) {
         runOnUiThread {
             screenText += "->:$text\n"
@@ -451,27 +264,25 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onDestroy() {
-        biomoduleBleManager.removeRawDataListener(rawDataListener)
-        cushionBleManager.removeRawDataListener(rawCushionDataListener)
-        biomoduleBleManager.removeHeartRateListener(hrDataListener)
         affectiveDataAnalysisService?.finishAffectiveService(object :
             IFinishAffectiveServiceListener {
             override fun finishAffectiveFail(error: Error?) {
-                TODO("Not yet implemented")
+                appendLog("算法初始化失败：${error}")
             }
 
             override fun finishBioFail(error: Error?) {
-                TODO("Not yet implemented")
+                appendLog("算法初始化失败：${error}")
             }
 
             override fun finishError(error: Error?) {
-                TODO("Not yet implemented")
+                appendLog("算法初始化失败：${error}")
             }
 
             override fun finishSuccess() {
-                TODO("Not yet implemented")
+                appendLog("结束服务成功")
             }
         })
+        affectiveDataAnalysisService?.closeAffectiveServiceConnection()
         super.onDestroy()
     }
 
